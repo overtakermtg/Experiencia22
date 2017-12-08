5aa43ed2d831f47e64ca053a4a513f9f6b086cf3
Overtaker <angel.overtaker@gmail.com>
2017-12-08 12:31:14 -0300

    cuarto commit

--- db/migrate/20171208000124_create_students.rb
+++ db/migrate/20171208000124_create_students.rb
@@ -1,8 +1,17 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
+
+
+#   Use this command at rails console: 
+#	rails generate migration add_fieldname_to_tablename fieldname:string
+#   and
+#   rake db:migrate 
+#   to run this migration
+
     create_table :students do |t|
       t.string :name
       t.integer :age
+      t.string :email
 
       t.timestamps
     end
